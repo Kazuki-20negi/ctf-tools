7145b6f204eb4049b559761f0c0c9e357620204f
20本ネギ <s.kazuki0111@gmail.com>
2026-01-18 01:12:28 +0900

    Update socket_solver_template.py

--- socket_solver_template.py
+++ socket_solver_template.py
@@ -2,6 +2,7 @@ from pwn import *
 import re
 
 # 各種設定
+context.log_level = 'debug'
 host="example.com"
 port=80
 io=remote(host, port)
@@ -9,8 +10,10 @@ text="Question:"
 
 def main():
     while True:
-        receive_text=io.recvuntil(text).decode()
+        receive_text=io.recvuntil(text).decode() #数式部を取り出す処理が必要
         io.sendline(four_operations(receive_text).encode())
 
 def four_operations(formula):
     return str(eval(formula))
+
+main()
