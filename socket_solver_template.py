e5e31029b92cf796460bbfa60bed95099abed7fc
20本ネギ <s.kazuki0111@gmail.com>
2026-01-18 01:04:10 +0900

    Update socket_solver_template.py

--- socket_solver_template.py
+++ socket_solver_template.py
@@ -1,1 +1,16 @@
 from pwn import *
+import re
+
+# 各種設定
+host="example.com"
+port=80
+io=remote(host, port)
+text="Question:"
+
+def main():
+    while True:
+        receive_text=io.recvuntil(text)
+        io.sendline(four_operations(receive_text))
+
+def four_operations(formula):
+    return eval(formula)
