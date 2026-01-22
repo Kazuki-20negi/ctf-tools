b09713c1a16575418fd4c01fee486c4aa29946ab
20本ネギ <s.kazuki0111@gmail.com>
2026-01-22 11:49:32 +0900

    Update socket_solver_template.py

--- socket_solver_template.py
+++ socket_solver_template.py
@@ -3,15 +3,15 @@ import re
 
 # 各種設定
 context.log_level = 'debug'
-host="example.com"
-port=80
+host="127.0.0.1"
+port=9999
 io=remote(host, port)
 text="Question:"
 
 def main():
     while True:
-        receive_text=io.recvuntil(text).decode()
-        clean_formula=re.sub(r"[^0-9+\-*/(). ]","",receive_text)
+        io.recvuntil(text)
+        clean_formula=io.recvline().decode().strip()
         io.sendline(four_operations(clean_formula).encode())
 
 def four_operations(formula):
