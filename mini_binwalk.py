f1ff6189c19e9b1ecd291542cd90853a2e283a16
20本ネギ <s.kazuki0111@gmail.com>
2026-01-26 20:28:13 +0900

    検知機能を追加

--- mini_binwalk.py
+++ mini_binwalk.py
@@ -8,3 +8,17 @@ SIGNATURES = { #マジックナンバー
     'GIF': b'GIF89a',                # GIF89a
 }
 
+target_file="test.jpg"
+
+with open(target_file, "rb") as f:
+    data=f.read()
+
+for file_type, magic_bytes in SIGNATURES.items():
+    offset=0
+    while True:
+        found_index= data.find(magic_bytes,offset)
+        if found_index==-1:
+            break
+        if found_index>0:
+            print(f"ヘッダを発見")
+            
